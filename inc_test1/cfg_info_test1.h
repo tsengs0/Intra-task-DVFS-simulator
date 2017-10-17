9c2f73df8b5925211aa2d2660247a1c3a93ceb8d
tsengs0 <tsengs0@gmail.com>
2017-10-17 13:56:25 +0900

    Move all checkpoints' relevant data structure to checkpoint_info.h

--- inc_test1/cfg_info_test1.h
+++ inc_test1/cfg_info_test1.h
@@ -7,6 +7,7 @@
 #include "jitter_info.h"
 #include "main.h"
 #include "timer.h"
+#include "checkpoint_info.h"
 
 using namespace std;
 
@@ -44,29 +45,6 @@ typedef struct L_Mining_table {
 	vector<int> taken_rwcec;
 	int successors[2];
 } L_mining_table_t;
-
-typedef vector< vector<int> > L_checkpoints_t;
-typedef vector<int> B_checkpoints_t;
-typedef vector<L_checkpoints_t> P_checkpoints_t;
-typedef vector<int> L_loop_iteration_t;
-typedef vector< vector<int> > P_loop_iteration_t;
-// The set of various types of Checkpoints
-typedef struct Checkpoints {
-	B_checkpoints_t B_checkpoints;
-	L_checkpoints_t L_checkpoints;
-	P_checkpoints_t P_checkpoints;
-	L_loop_iteration_t L_loop_iteration; // Recording the number of iteration every loop nest have run so far
-	P_loop_iteration_t P_loop_iteration;
-} checkpoints_t;
-
-typedef int B_type[4];
-typedef int L_type[2][8];
-typedef int P_type[3];
-typedef struct RWCEC_Trace {
-	B_type *B_RWCEC_t;
-	L_type *L_RWCEC_t;
-	P_type *P_RWCEC_t; 
-} RWCEC_Trace_in;
 
 typedef struct isr_context{
 	float act_exe_time;
--- inc_test1/checkpoint_info.h
+++ inc_test1/checkpoint_info.h
@@ -1,6 +1,29 @@
 #ifndef __CHECKPOINT_INFO_H
 #define __CHECKPOINT_INFO_H
 
+typedef vector<int> B_checkpoints_t;
+typedef vector< vector<int> > L_checkpoints_t;
+typedef vector<int> P_checkpoints_t;
+typedef vector<int> L_loop_iteration_t;
+typedef vector<int> P_loop_iteration_t;
+// The set of various types of Checkpoints
+typedef struct Checkpoints {
+	B_checkpoints_t B_checkpoints;
+	L_checkpoints_t L_checkpoints;
+	P_checkpoints_t P_checkpoints;
+	L_loop_iteration_t L_loop_iteration; // Recording the number of iteration every loop nest have run so far
+	P_loop_iteration_t P_loop_iteration;
+} checkpoints_t;
+
+typedef int B_type[4];
+typedef int L_type[2][8];
+typedef int P_type[3];
+typedef struct RWCEC_Trace {
+	B_type *B_RWCEC_t;
+	L_type *L_RWCEC_t;
+	P_type *P_RWCEC_t; 
+} RWCEC_Trace_in;
+
 typedef struct Checkpoint_Num {
 	int B_ch;
 	int L_ch;
